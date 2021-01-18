2e96d068f1ecab983147e110873b27ff4bc5836b
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-01-19 01:01:57 +0700

    add new configure and update configure

--- pages/projects/_projectSlug/configures/_configureId.vue
+++ pages/projects/_projectSlug/configures/_configureId.vue
@@ -6,74 +6,170 @@
 
     <div class="container" v-else>
       <div class="header" style="margin:20px 0px">
-        <p class="title">Configuration Description</p>
+        <div class="columns is-multiline">
+          <div class="column is-10">
+            <p class="title">Configuration Description</p>
+          </div>
+          <div class="column is-2">
+            <b-button type="is-primary" expanded :icon-left="isNew ? 'plus' : 'pencil'" @click="onSubmit()">
+              {{ isNew ? 'Save Configure' : 'Update Configure' }}
+            </b-button>
+          </div>
 
-          <b-input v-model="configure.description"  type="textarea"></b-input>
+          <div class="column is-12">
+            <b-input v-model="configure.description" type="textarea"></b-input>
+          </div>
+        </div>
+
 
       </div>
 
       <Editor :prop-code="configure.config" v-on:on-change-code="onChangeCode"/>
+
+
     </div>
   </section>
 </template>
 
 <script>
+import {mapMutations} from 'vuex';
 import {showToast} from "@/services/utils";
 import Editor from "@/components/Editor";
+
 export default {
-  layout : 'nav',
-  components : {
+  layout: 'nav',
+  components: {
     Editor
   },
-  data(){
-    return{
-      isLoading : false,
-        configure : {
-          _id : null,
-          projectId : null,
-          config : null,
-          description : null,
+  data() {
+    return {
+      isNew: this.$route.params.configureId === 'new',
+      isLoading: false,
+      configure: {
+        _id: null,
+        projectId: null,
+        config: {
+          request: {
+            destination_url: "",
+            destination_path: "",
+            method: "",
+            transform: "",
+            log_before_modify: "",
+            log_after_modify: "",
+            adds: {
+              header: {},
+              body: {},
+              query: {}
+            },
+            modifies: {
+              header: {},
+              body: {},
+              query: {}
+            },
+            deletes: {
+              header: [],
+              body: [],
+              query: []
+            }
+          },
+          response: {
+            transform: "",
+            log_before_modify: "",
+            log_after_modify: "",
+            adds: {
+              header: {},
+              body: {},
+              query: {}
+            },
+            modifies: {
+              body: {}
+            },
+            deletes: {
+              body: []
+            }
+          }
         },
-      description : null,
+        description: null,
+      },
+
     }
   },
-  methods : {
-    onChangeCode(val){
+  methods: {
+    ...mapMutations({
+      setSelectedProject: 'projects/setSelectedProject'
+
+    }),
+    onChangeCode(val) {
 
       this.configure.config = val;
 
       console.log(this.configure)
     },
-    async loadConfigure(){
-      const {projectSlug,configureId} = this.$route.params
+    async loadConfigure() {
+      const {projectSlug, configureId} = this.$route.params
 
       this.isLoading = true;
-      try{
-        let res = await this.$axios.get(`/configure`,{
-          params : {
+      try {
+        let res = await this.$axios.get(`/configure`, {
+          params: {
             projectSlug,
             configureId
           }
         })
-        const {configure,description} = res.data;
+        const {config,description} = res.data;
 
-
-        if(configure){
-          this.configure = configure;
+        if (config) {
+          this.configure.config = config;
+          this.configure.description = description;
         }
 
-        if(description){
-          this.description = description
-        }
-
-      }catch (err) {
+      } catch (err) {
         showToast(err.response.data.message)
       }
       this.isLoading = false;
-    }
+    },
+    async newConfigure() {
+      const {projectSlug, } = this.$route.params;
+      try {
+        let response = this.$axios.post(`/configure`, {
+          projectSlug,
+          description : this.configure.description,
+          config: this.configure.config
+        });
+        this.setSelectedProject(response.data)
+        this.$router.go(-1)
+      } catch (err) {
+        showToast(err.response.data.message, 'is-danger', 'is-top')
+      }
+    },
+    async updateConfigure() {
+      const {projectSlug, configureId} = this.$route.params;
+      try {
+        let response = this.$axios.put(`/configure?projectSlug=${projectSlug}&configureId=${configureId}`, {
+
+          description : this.configure.description,
+          config: this.configure.config
+        });
+        this.setSelectedProject(response.data)
+        this.$router.go(-1)
+      } catch (err) {
+        showToast(err.response.data.message, 'is-danger', 'is-top')
+      }
+    },
+    async onSubmit() {
+      if(this.isNew) {
+        await this.newConfigure();
+      }else{
+        await this.updateConfigure();
+      }
+    },
   },
   async created() {
-    await this.loadConfigure();
+
+    if (!this.isNew) {
+      await this.loadConfigure();
+    }
+
   }
 }
 </script>
