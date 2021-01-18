3528f31931f7905c192791cf4ac41009dfc96794
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-01-19 01:04:07 +0700

    add button to add new project, delete project

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,63 +1,94 @@
 <template>
   <section class="section">
     <div v-if="isLoading">
-      <b-loading :is-full-page="isFullPageLoading" :active="isLoading"  ></b-loading>
+      <b-loading :is-full-page="isFullPageLoading" :active="isLoading"></b-loading>
+    </div>
+
+    <div v-else>
+      <div class="columns is-multiline">
+        <div class="column is-12">
+          <p class="is-size-1 has-text-weight-bold"> Your Projects</p>
+        </div>
+        <div class="column is-12">
+          <b-button type="is-primary has-text-weight-bold" @click="isComponentModalActive = true">Add New Project
+          </b-button>
+        </div>
+      </div>
+
+      <masonry
+        :cols="{default: 3, 1000: 2, 700: 1, 400: 1}"
+        :gutter="{default: '30px', 700: '15px'}"
+
+      >
+        <div v-for="project in projects" :key="project._id" style="padding: 10px">
+          <card
+            :title="project.name"
+            icon="folder"
+
+          >
+            <template v-slot:card-content v-if="project.description">
+
+
+              <div>
+                <span class="has-text-weight-semibold"> Project Slug : </span>
+                <p class="dont-break-out">{{ project.slug }} </p>
+                <span class="has-text-weight-semibold">Project Description : </span>
+                <p class="has-text-weight-normal dont-break-out">{{ project.description }}</p>
+              </div>
+
+
+            </template>
+            <template v-slot:card-content v-else>
+              No project description provided
+            </template>
+            <template v-slot:card-footer-content>
+              <nuxt-link :to="`/projects/${project.slug}`" class="card-footer-item">
+
+                <b-icon
+
+                  icon="pencil"
+                  size="is-small"
+                >
+                </b-icon>
+              </nuxt-link>
+              <a @click="deleteProject(project.slug)" class="card-footer-item">
+
+                <b-icon
+
+                  icon="delete"
+                  size="is-small"
+                >
+                </b-icon>
+              </a>
+            </template>
+          </card>
+
+
+        </div>
+      </masonry>
+
     </div>
 
 
-    <masonry
-      :cols="{default: 3, 1000: 2, 700: 1, 400: 1}"
-      :gutter="{default: '30px', 700: '15px'}"
-      v-else
-    >
-      <div v-for="project in projects" :key="project._id" style="padding: 10px">
-        <card
-          :title="project.name"
-          icon="folder"
+    <b-modal
 
-        >
-          <template v-slot:card-content v-if="project.description">
+      v-model="isComponentModalActive"
+      has-modal-card
+      trap-focus
+      :destroy-on-hide="false"
+      aria-role="dialog"
+      aria-label="Example Modal"
 
+      aria-modal>
 
-            <div>
-              <span class="has-text-weight-semibold"> Project Slug : </span>
-              <p class="dont-break-out">{{ project.slug }} </p>
-              <span class="has-text-weight-semibold">Project Description : </span>
-              <p class="has-text-weight-normal dont-break-out">{{ project.description }}</p>
-            </div>
+      <template #default="props">
+        <div >
+          <FormEditProject :project-prop="newProject" v-on:close="isComponentModalActive = false"
+                           v-if="isComponentModalActive"   mode="add" />
+        </div>
 
-
-
-          </template>
-          <template v-slot:card-content v-else>
-            No project description provided
-          </template>
-          <template v-slot:card-footer-content>
-            <nuxt-link :to="`/projects/${project.slug}`" class="card-footer-item">
-
-              <b-icon
-
-                icon="pencil"
-                size="is-small"
-              >
-              </b-icon>
-            </nuxt-link>
-            <a @click="deleteProject" class="card-footer-item">
-
-              <b-icon
-
-                icon="delete"
-                size="is-small"
-              >
-              </b-icon>
-            </a>
-          </template>
-        </card>
-
-
-      </div>
-    </masonry>
-
+      </template>
+    </b-modal>
 
   </section>
 </template>
@@ -66,12 +97,13 @@
 import Card from '~/components/Card'
 import {mapActions, mapGetters} from "vuex";
 import {showToast} from "~/services/utils";
-
+import FormEditProject from "@/components/FormEditProject";
 
 export default {
   name: 'HomePage',
   components: {
-    Card
+    Card,
+    FormEditProject
   },
   computed: {
     ...mapGetters({
@@ -80,16 +112,30 @@ export default {
   },
   data() {
     return {
+      isComponentModalActive: false,
       isLoading: false,
       isFullPageLoading: false,
+      newProject: {
+        name: null,
+        description: null,
+      },
     }
   },
   methods: {
     ...mapActions({
-      getProjects: "projects/fetchProjects"
+      getProjects: "projects/fetchProjects",
+      deleteProjectBySlug: 'projects/deleteProjectBySlug'
     }),
-    deleteProject() {
-      console.log("delete project triggered")
+    deleteProject(slug) {
+      this.$buefy.dialog.confirm({
+        title: `Deleting project ${slug}`,
+        message: 'Are you sure you want to <b>delete</b> this project This action cannot be undone.',
+        confirmText: 'Delete Project',
+        type: 'is-danger',
+        hasIcon: true,
+        onConfirm: async () => await this.deleteProjectBySlug(slug),
+
+      })
     },
     async loadProjects() {
 
@@ -102,8 +148,9 @@ export default {
         showToast(err.response.data.message, 'is-danger', 'is-bottom')
       }
       this.isLoading = false;
+      this.$emit('close');
+    },
 
-    }
   },
 
   async created() {
@@ -116,6 +163,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+
 .grid-item {
   min-width: 200px;
   //img{
