2f89e317e1317e3e3891bf358b02ce7985ee8901
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-01-19 01:02:53 +0700

    add action and mutation to save project, delete project

--- store/projects.js
+++ store/projects.js
@@ -1,17 +1,29 @@
 export const state = () => ({
   projects: [],
-  selectedProject : null,
+  selectedProject: null
 })
 
 export const getters = {
   getProjects: state => {
     return state.projects
   },
-  getProjectBySlug : state=> {
+  getSelectedProject: state => {
     return state.selectedProject;
   }
 }
 export const actions = {
+  async saveProject(context, data) {
+    return new Promise(((resolve, reject) => {
+      this.$axios.post('/project', data)
+        .then(response => {
+
+          if (response.status === 200) {
+            context.commit("addNewProject", response.data)
+          }
+          resolve(response)
+        }).catch(error => reject(error));
+    }))
+  },
   async fetchProjects(context) {
     return new Promise(((resolve, reject) => {
       this.$axios.get('/project')
@@ -25,24 +37,47 @@ export const actions = {
     }))
   },
   // Get project detail
-  async fetchProjectBySlug(context,projectSlug){
+  async fetchProjectBySlug(context, projectSlug) {
     return new Promise(((resolve, reject) => {
       this.$axios.get(`/project/${projectSlug}`)
         .then(response => {
           if (response.status === 200) {
-            context.commit("setSelectedProject", response.data)
+            const {project} = response.data
+
+            context.commit("setSelectedProject", project)
           }
+          resolve(response)
+        }).catch(error => reject(error));
+    }))
+  },
+
+  // Delete project detail
+  async deleteProjectBySlug(context, projectSlug) {
+    return new Promise(((resolve, reject) => {
+      this.$axios.delete(`/project/${projectSlug}`)
+        .then(response => {
+
+          context.commit("removeProjectBySlug", projectSlug)
+
           resolve(response)
         }).catch(error => reject(error));
     }))
   }
 }
 export const mutations = {
+  addNewProject(state, data) {
+    // Push to first element
+    state.projects = [data, ...state.projects];
+  },
   setProjects(state, data) {
     state.projects = data;
   },
-  setSelectedProject(state,data){
+  setSelectedProject(state, data) {
     state.selectedProject = data;
-  }
+  },
+  removeProjectBySlug(state, slug) {
+    state.projects = [...state.projects.filter(element => element.slug !== slug)]
+  },
+
 
 }
