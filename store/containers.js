e6ae7f923e215d3b736dc46beca35897504226b1
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-01-31 21:21:27 +0700

    add function to toggle status container and create container

--- store/containers.js
+++ store/containers.js
@@ -66,12 +66,41 @@ export const actions = {
           resolve(response)
         }).catch(error => reject(error));
     }))
+  },
+  async toggleStatusContainer(context,data){
+    const {id} = data;
+    return new Promise(((resolve, reject) => {
+      this.$axios.put(`/container/docker-container`, null, {
+        params : {
+          id
+        }
+      })
+        .then(response => {
+          resolve(response)
+        }).catch(error => reject(error));
+    }))
+  },
+  async createDockerContainer(context,id){
+    return new Promise(((resolve, reject) => {
+      this.$axios.post(`/container/docker-container`, null, {
+        params : {
+          id
+        }
+      })
+        .then(response => {
+          resolve(response)
+        }).catch(error => reject(error));
+    }))
   }
 
 }
 export const mutations = {
   setContainers(state,data){
     state.containers = data;
+  },
+  updateSpecificContainer(state,data){
+    let updatedIndex = state.containers.findIndex((e) => e.id === data.id)
+    state.containers[updatedIndex] = {...data};
   },
   setSelectedContainer(state,data){
     state.selectedContainer = data;
