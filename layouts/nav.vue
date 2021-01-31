6beda53da4dba244ddd51cb0196ef7839f85507c
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-01-31 21:20:55 +0700

    use buefy navbar. add logout

--- layouts/nav.vue
+++ layouts/nav.vue
@@ -1,31 +1,55 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/dashboard"
-        >
+    <b-navbar fixed-top type="is-primary">
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
           <img
-            src="~assets/buefy.png"
-            alt="Buefy"
-            height="28"
+            src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+            alt="Lightweight UI components for Vue.js based on Bulma"
           >
-        </a>
+        </b-navbar-item>
+      </template>
+      <template #start>
+        <b-navbar-item href="#">
+          Home
+        </b-navbar-item>
+        <b-navbar-item href="#">
+          Documentation
+        </b-navbar-item>
+        <b-navbar-dropdown label="Info">
+          <b-navbar-item href="#">
+            About
+          </b-navbar-item>
+          <b-navbar-item href="#">
+            Contact
+          </b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
 
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
+      <template #end>
+        <b-navbar-item tag="div">
+          <div class="buttons">
+
+            <b-button type="is-danger" @click="logOut()">
+               Log Out
+            </b-button>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
 
     <Nuxt/>
   </div>
 
 </template>
+
+<script>
+export default  {
+  methods : {
+    async logOut(){
+
+      await this.$auth.logout()
+    }
+  }
+}
+</script>
